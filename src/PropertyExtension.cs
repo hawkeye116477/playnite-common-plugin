44a264cb0e16a8be01c581c246a5e2cb49bca2f4
hawkeye116477 <hawkeye116477@gmail.com>
2024-11-02 15:33:02 +0100

    Add common things

---
+++ src/PropertyExtension.cs
@@ -0,0 +1,10 @@
+﻿namespace CommonPlugin
+{
+    public static class PropertyExtension
+    {
+        public static object GetPropertyValue(this object T, string PropName)
+        {
+            return T.GetType().GetProperty(PropName)?.GetValue(T, null);
+        }
+    }
+}
