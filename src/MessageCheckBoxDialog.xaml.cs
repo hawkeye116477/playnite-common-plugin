053ed320ef97d17d58843662bacf684fec18d268
hawkeye116477 <hawkeye116477@gmail.com>
2025-08-16 17:43:27 +0200

    Fluent revolution - part 1
    For now PF handler for C# code, next will be WPF markup.

--- src/MessageCheckBoxDialog.xaml.cs
+++ src/MessageCheckBoxDialog.xaml.cs
@@ -64,7 +64,7 @@ namespace CommonPlugin
             }
             if (messageBoxText?.StartsWith("LOC", StringComparison.Ordinal) == true)
             {
-                MessageText = ResourceProvider.GetString(messageBoxText);
+                MessageText = LocalizationManager.Instance.GetString(messageBoxText);
             }
             else
             {
@@ -72,7 +72,7 @@ namespace CommonPlugin
             }
             if (checkBoxText?.StartsWith("LOC", StringComparison.Ordinal) == true)
             {
-                CheckBoxText = ResourceProvider.GetString(checkBoxText);
+                CheckBoxText = LocalizationManager.Instance.GetString(checkBoxText);
             }
             else
             {
