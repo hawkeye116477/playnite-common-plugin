7745af45b04a0a2f15244a080d1f8473c1baf547
hawkeye116477 <hawkeye116477@gmail.com>
2026-08-12 14:48:23 +0200

    Use new lib for keyring
    AdysTech.CredentialManager instead of GnomeStack.Secrets.OperatingSystem
    While earlier one was multi-platform, it seems it was also buggy, could fetch or save incorrectly. So I'll use more modern one lib only for Windows and then in the future maybe https://github.com/Ace4896/DBus.Services.Secrets for Linux.

--- src/Keyring.cs
+++ src/Keyring.cs
@@ -1,6 +1,5 @@
-using System.Runtime.InteropServices;
-using GnomeStack.Secrets.Win32;
-using GnomeStack.Standard;
+using System.Net;
+using AdysTech.CredentialManager;
 
 namespace CommonPlugin;
 
@@ -8,23 +7,18 @@ public class Keyring
 {
     public static void SetPassword(string servicename, string username, string password)
     {
-        if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
-        {
-            WinCredManager.SetSecret(servicename, username, password, false, null, WinCredPersistence.LocalMachine);
-        }
-        else
-        {
-            OsSecretVault.SetSecret(servicename, username, password);
-        }
+        var credential = new NetworkCredential(username, password);
+        CredentialManager.SaveCredentials(servicename + "/" + username, credential);
     }
 
     public static string? GetPassword(string servicename, string username)
     {
-        return OsSecretVault.GetSecret(servicename, username);
+        var storedCredential = CredentialManager.GetCredentials(servicename + "/" + username);
+        return storedCredential?.Password;
     }
     
     public static void DeletePassword(string servicename, string username)
     {
-        OsSecretVault.DeleteSecret(servicename, username);
+        CredentialManager.RemoveCredentials(servicename + "/" + username);
     }
 }
