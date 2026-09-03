06b90cf70c279cbf19b8133008cedcca4dae7932
hawkeye116477 <hawkeye116477@gmail.com>
2026-09-03 17:48:39 +0200

    Add new icons and WPF markup

---
+++ src/IconExtension.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Windows.Markup;
+
+namespace CommonPlugin;
+
+public class IconExtension : MarkupExtension
+{
+    public IconExtension(string code)
+    {
+        Code = code;
+    }
+
+    public IconExtension(string code, string text)
+    {
+        Code = code;
+        Text = text;
+    }
+
+    public IconExtension()
+    {
+    }
+
+    public string? Code { get; set; }
+    public string? Text { get; set; }
+        
+    public override object? ProvideValue(IServiceProvider serviceProvider)
+    {
+        var finalResult = "";
+        if (Code != null)
+        {
+            finalResult = char.ConvertFromUtf32(int.Parse(Code, System.Globalization.NumberStyles.HexNumber));
+            if (!string.IsNullOrEmpty(Text))
+            {
+                finalResult += $" {Text}";
+            }
+        }
+        return finalResult;
+    }
+}
--- src/Resources/CommonIcons.cs
+++ src/Resources/CommonIcons.cs
@@ -4,15 +4,31 @@ namespace CommonPlugin.Resources
 {
     public static class CommonIcons
     {
-        public static UIIcon RepairIcon { get; } = UIIcon.FromFontIcon("efd2", Fonts.IcoFont);
-        public static UIIcon MoveIcon { get; } = UIIcon.FromFontIcon("ea5d", Fonts.IcoFont);
-        public static UIIcon ModifyLaunchSettingsIcon { get; } = UIIcon.FromFontIcon("efe2", Fonts.IcoFont);
-        public static UIIcon InstallIcon { get; } = UIIcon.FromFontIcon("ef08", Fonts.IcoFont);
-        public static UIIcon UninstallIcon { get; } = UIIcon.FromFontIcon("edec", Fonts.IcoFont);
+        public static string Repair => "e20f";
+        public static string Move => "f0252";
+        public static string Install => "f019";
+        public static string Uninstall => "f19d6";
+        public static string FinishInstallation => "f05d";
+        public static string Update => "f021";
+        public static string ImportGame => "e27d";
+        public static string Dlcs => "f12e";
+        
+        public static string IcoInstall => "ef07";
+        public static string Reload => "efd1";
+        public static string Cancel => "ec4f";
+        public static string SelectFolder => "ec5b";
+        public static string IcoRepair => "efd2";
+        public static string CheckAll => "eed9";
+        
+        public static UIIcon RepairIcon { get; } = UIIcon.FromFontIcon(Repair, Fonts.NerdFont);
+        public static UIIcon MoveIcon { get; } = UIIcon.FromFontIcon(Move, Fonts.NerdFont);
+        public static UIIcon InstallIcon { get; } = UIIcon.FromFontIcon(Install, Fonts.NerdFont);
+        public static UIIcon UninstallIcon { get; } = UIIcon.FromFontIcon(Uninstall, Fonts.NerdFont);
         public static UIIcon ExtrasIcon { get; } = UIIcon.FromFontIcon("ef3c", Fonts.IcoFont);
-        public static UIIcon FinishInstallationIcon { get; } = UIIcon.FromFontIcon("eed7", Fonts.IcoFont);
-        public static UIIcon UpdateIcon { get; } = UIIcon.FromFontIcon("efd1", Fonts.IcoFont);
-        public static UIIcon ImportGameIcon { get; } = UIIcon.FromFontIcon("ec3e", Fonts.IcoFont);
+        public static UIIcon FinishInstallationIcon { get; } = UIIcon.FromFontIcon(FinishInstallation, Fonts.NerdFont);
+        public static UIIcon UpdateIcon { get; } = UIIcon.FromFontIcon(Update, Fonts.NerdFont);
+        public static UIIcon ImportGameIcon { get; } = UIIcon.FromFontIcon(ImportGame, Fonts.NerdFont);
+        public static UIIcon DlcsIcon { get; } = UIIcon.FromFontIcon(Dlcs, Fonts.NerdFont);
     }
 }
 
