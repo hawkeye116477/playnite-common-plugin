656be0a8846205a7b8f1dc5553715ed8df4fa7bf
hawkeye116477 <hawkeye116477@gmail.com>
2026-09-03 22:58:51 +0200

    Add WindowsNotifyIconManager

---
+++ third_party/Playnite_mod/WindowsNotifyIconManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace PlayniteMod
+{
+    public static class WindowsNotifyIconManager
+    {
+        public static void Notify(Icon icon, string title, string body, Action? clickAction = null)
+        {
+            var notifyIcon = new NotifyIcon
+            {
+                Icon = icon,
+                BalloonTipTitle = title,
+                BalloonTipText = body,
+                Visible = true
+            };
+            notifyIcon.BalloonTipClicked += (o, ea) => { clickAction?.Invoke(); notifyIcon.Dispose(); };
+            notifyIcon.BalloonTipClosed += (o, ea) => { notifyIcon.Dispose(); };
+            notifyIcon.ShowBalloonTip(60); // Windows Vista and up timeout is 5sec by default, only Windows Accessibility Settings can override this
+        }
+    }
+}
